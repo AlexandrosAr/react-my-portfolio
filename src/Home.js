ef798fe5941fd0395546caf5ec84c200cb0416b9
aarapidis <aarapidis@deloitte.gr>
2020-01-08 16:22:51 +0200

    1st home page

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Home.css'
+/*import logo from './a2png.png' */
+
+export class Home extends Component  {
+    
+    render() {
+        return (
+            <div id='home-container'>
+                <h1>Alexandros Arapidis</h1>
+            </div>
+        );
+    }
+    
+}
+
+export default Home;
