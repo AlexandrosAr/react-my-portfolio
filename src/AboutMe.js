c7a844021ebab42ede5deb691f195e9a1491e726
aarapidis <aarapidis@deloitte.gr>
2020-01-08 16:23:54 +0200

    [1] About me page

---
+++ src/AboutMe.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+export class AboutMe extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>About Me</h1>
+            </div>
+        );
+    }
+    
+}
+
+export default AboutMe;
