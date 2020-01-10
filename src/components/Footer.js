53aaeeea070276ae22dc3c9ae51eb924e856f09b
aarapidis <aarapidis@deloitte.gr>
2020-01-10 11:48:21 +0200

    [1]Footer creation

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Footer.css'
+
+
+
+export class Footer extends React.Component {
+
+    render() {
+        return (
+            <div id='footer-container'>
+                My Footer
+            </div>
+        );
+    }
+
+}
+
+export default Footer;
+
