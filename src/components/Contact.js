dbb029eb35be85559cf0928a3781c8bb49b1c65c
aarapidis <aarapidis@deloitte.gr>
2020-01-15 12:13:06 +0200

    [1]New page about - initial

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Contact.css';
+
+class Contact extends Component {
+    render() {
+        return(
+            <div id='contact-container'>
+                To get in touh with me make a pray to the God!
+            </div>
+        );
+    }
+}
+
+export default Contact;
