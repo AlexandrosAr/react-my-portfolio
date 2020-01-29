8d916bd54b6204a5dde66d49fb9f5ac9215954ac
aarapidis <aarapidis@deloitte.gr>
2020-01-29 17:03:09 +0200

    [1]Home page developments

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './Home.css'
-/*import logo from './a2png.png' */
+import logo from '../imgs/profile_pic.jpg'
 
-export class Home extends Component  {
-    
+export class Home extends Component {
+
     render() {
         return (
             <div id='home-container'>
-                <h1>Alexandros Arapidis</h1>
+                <div id='inside-square'>
+                    <div id='img-title-container'>
+                        <img className='custom-img' src={logo} alt='website logo' height='240' width='240' ></img>
+                        <div id='custom-title'>
+                            <h1>Alexandros Arapidis</h1>
+                            <p>Junior Software Engineer</p>
+                        </div>
+                    </div>
+
+                    <div id='introduction-container'>
+                        <p>Hi guys! Welcome to my personal portfolio!</p>
+                        <p>My name is Alex, I am 29 year old and I am a passionate developer who seeks always to improve himself.</p>
+                        <p>This is my first attempt to develop a portfolio.</p>
+                        <p>Feel free to get in touch with me.</p>
+                        <p>Thank you!</p>
+                    </div>
+                </div>
+
             </div>
         );
     }
-    
+
 }
 
 export default Home;
