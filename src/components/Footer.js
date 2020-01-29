01194502bd4dadfdf489b23c2e3071c1ce3e7d51
aarapidis <aarapidis@deloitte.gr>
2020-01-29 17:45:37 +0200

    [1] Dynamic date and age impl

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,12 @@ import './Footer.css'
 export class Footer extends React.Component {
 
     render() {
+        let d = new Date();
+        let date =  d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
+        
         return (
             <div id='footer-container'>
-                Developed by Alexandros Arapidis, React.js
+                Developed by Alexandros Arapidis, React.js, {date}
             </div>
         );
     }
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,13 @@ import logo from '../imgs/profile_pic.jpg'
 export class Home extends Component {
 
     render() {
+
+        /* Calculates my Age in Years */
+        let birthdate = new Date('7/7/1990');
+        let d = new Date();
+        let currentDate = new Date(d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate());
+        let dynamicAge = parseInt(Math.floor((Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()) - Date.UTC(birthdate.getFullYear(), birthdate.getMonth(), birthdate.getDate())) / (1000 * 60 * 60 * 24 * 365)), 10);
+
         return (
             <div id='home-container'>
                 <div id='inside-square'>
@@ -13,13 +20,13 @@ export class Home extends Component {
                         <img className='custom-img' src={logo} alt='website logo' height='240' width='240' ></img>
                         <div id='custom-title'>
                             <h1>Alexandros Arapidis</h1>
-                            <p>Junior Software Engineer</p>
+                            <p>Software Engineer</p>
                         </div>
                     </div>
 
                     <div id='introduction-container'>
                         <p>Hi guys! Welcome to my personal portfolio!</p>
-                        <p>My name is Alex, I am 29 year old and I am a passionate developer who seeks always to improve himself.</p>
+                        <p>My name is Alex, I am {dynamicAge} years old and I am a passionate developer who seeks always to improve himself.</p>
                         <p>This is my first attempt to develop a portfolio.</p>
                         <p>Feel free to get in touch with me.</p>
                         <p>Thank you!</p>
