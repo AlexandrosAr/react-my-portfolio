78f615ebcb7f269f746afdfd8de0a79714e50229
aarapidis <aarapidis@deloitte.gr>
2020-02-07 17:33:35 +0200

    [1]Social Media links and funcionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,17 +3,88 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './Contact.css';
 import ContactMeComponent from './ContactMeComponent'
 
+import  linkedin from '../imgs/social-media-png/linkedin.png';
+import facebook from '../imgs/social-media-png/facebook.png';
+import instagram from '../imgs/social-media-png/instagram.png';
+import whatsapp from '../imgs/social-media-png/whatsapp.png';
+import twitter from '../imgs/social-media-png/twitter.png';
+
+
+import linkedinHover from '../imgs/social-media-png/linkedin-hover.png';
+import facebookHover from '../imgs/social-media-png/facebook-hover.png';
+import instagramHover from '../imgs/social-media-png/instagram-hover.png';
+import whatsappHover from '../imgs/social-media-png/whatsapp-hover.png';
+import twitterHover from '../imgs/social-media-png/twitter-hover.png';
+
+
+
 class Contact extends Component {
+
+    constructor(props) {
+        super(props);
+
+        const socialMediaIconMap = new Map([['linkedin', linkedin], ['facebook', facebook], ['instagram', instagram], ['whatsapp', whatsapp], ['twitter', twitter], ['linkedinHover', linkedinHover], ['facebookHover', facebookHover], ['instagramHover', instagramHover], ['whatsappHover', whatsappHover] , ['twitterHover', twitterHover]]);
+
+        this.state = {
+            linkedin: linkedin,
+            facebook: facebook,
+            instagram: instagram,
+            whatsapp: whatsapp,
+            twitter: twitter,
+            linkMap : socialMediaIconMap
+        }   
+        this.handleMouseOver = this.handleMouseOver.bind(this);
+        this.handleMouseOut = this.handleMouseOut.bind(this);
+    }
+
+    handleMouseOver(e) {
+        const value = this.state.linkMap.get(e.target.id + 'Hover');
+        this.setState({
+            [e.target.id] : value
+        });
+    }
+
+    handleMouseOut(e) {
+        const value = this.state.linkMap.get(e.target.id);
+        this.setState({
+            [e.target.id] : value
+        });
+    }
+
     render() {
+
+        const fbDynamicLink = 'https://www.facebook.com/alexandros.arapidis';
+        const liDynamicLink = 'https://www.linkedin.com/in/alexandros-arapidis';
+        const inDynamicLink = 'https://www.instagram.com/alexandrosarapidis/';
+        const whDynamicLink = 'https://we.me/';
+        const twDynamicLink = 'https://twitter.com/Kozer07';
+
         return (
             <div id='page-container'>
                 <div id='inside-square'>
                     <div id='title'>
                         Contact Me!
                         <p>I'll be glad to answer your questions!</p>
-                        <ContactMeComponent />
                     </div>
-
+                    <ContactMeComponent />
+                    
+                    <div className='social-media-container'>
+                        <a href={liDynamicLink} target='_blank' rel='noopener noreferrer'>
+                            <img id='linkedin' className='social-media-img linkedin' src={this.state.linkedin} alt='linkedin btn' height='50px' onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut} ></img>
+                        </a>
+                        <a href={fbDynamicLink} target='_blank' rel='noopener noreferrer'>
+                            <img id='facebook' className='social-media-img button' src={this.state.facebook} alt='facebook btn' height='50px' onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut} ></img>
+                        </a>
+                        <a href={inDynamicLink} target='_blank' rel='noopener noreferrer'>
+                            <img id='instagram' className='social-media-img button' src={this.state.instagram} alt='facebook btn' height='50px' onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut} ></img>
+                        </a>
+                        <a href={whDynamicLink} target='_blank' rel='noopener noreferrer'>
+                            <img id='whatsapp' className='social-media-img button' src={this.state.whatsapp} alt='facebook btn' height='50px' onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut} ></img>
+                        </a>
+                        <a href={twDynamicLink} target='_blank' rel='noopener noreferrer'>
+                            <img id='twitter' className='social-media-img button' src={this.state.twitter} alt='facebook btn' height='50px' onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut} ></img>
+                        </a>
+                    </div>
                 </div>
 
             </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,59 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './Footer.css'
-import  linkedin from '../imgs/socialMediaLogos/png/027-linkedin.png';
-import facebook from '../imgs/036-facebook-footer.png';
+
+import github from '../imgs/github.png';
+import codepen from '../imgs/codepen.png';
+import githubHover from '../imgs/github-hover.png';
+import codepenHover from '../imgs/codepen-hover.png';
 
 
 
 export class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+        const imgLinkMap = new Map([['github', github], ['codepen', codepen], ['githubHover', githubHover], ['codepenHover', codepenHover]])
+        this.state = {
+            github: github,
+            codepen: codepen,
+            imgLinkMap: imgLinkMap
+        }
+
+        this.handleMouseOver = this.handleMouseOver.bind(this);
+        this.handleMouseOut = this.handleMouseOut.bind(this);
+    }
+
+    handleMouseOver(e) {
+        const value = this.state.imgLinkMap.get(e.target.id + 'Hover');
+        this.setState({
+            [e.target.id] : value
+        });
+    }
+
+    handleMouseOut(e) {
+        const value = this.state.imgLinkMap.get(e.target.id);
+        this.setState({
+            [e.target.id] : value
+        });
+    }
+
 
     render() {
-        let d = new Date();
-        let date =  d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
-        let fbDynamicLink = 'https://www.facebook.com/alexandros.arapidis';
-        let liDynamicLink = 'https://www.linkedin.com/in/alexandros-arapidis';
+        const d = new Date();
+        const date =  d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
+        const gitDynamicLink = 'https://github.com/AlexandrosAr';
+        const codepenDynamicLink = 'https://codepen.io/AlexAr';
+
         
         return (
             <div id='footer-container'>
                 <div>&copy;Developed by Alexandros Arapidis, React.js, {date} </div>
                 <div className='footer-social-media-container'>
-                    <a href={liDynamicLink} target='_blank' rel='noopener noreferrer'>
-                        <img className='social-media-img' src={linkedin} alt='linkedin btn' height='20px' ></img>
+                    <a href={gitDynamicLink} target='_blank' rel='noopener noreferrer'>
+                        <img id='github' className='social-media-img' src={this.state.github} alt='linkedin btn' height='20px' onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}></img>
                     </a>
-                    <a href={fbDynamicLink} target='_blank' rel='noopener noreferrer'>
-                        <img className='social-media-img button' src={facebook} alt='facebook btn' height='20px' ></img>
+                    <a href={codepenDynamicLink} target='_blank' rel='noopener noreferrer'>
+                        <img id='codepen' className='social-media-img button' src={this.state.codepen} alt='facebook btn' height='20px' onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}></img>
                     </a>
                 </div>
 
